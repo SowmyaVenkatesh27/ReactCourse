3dc9f87c9737547f29310a89ce73a17f2dfebd9f
SowmyaVenkatesh27 <121352482+SowmyaVenkatesh27@users.noreply.github.com>
2023-02-22 23:49:12 -0800

    Delete BasicReactProgramCDN directory

--- BasicReactProgramCDN/App.js
+++
@@ -1,5 +0,0 @@
-const heading1 = React.createElement("h1",{style:{color:"blue"}},"Namasate H1");
-const heading2 = React.createElement("h2",{style:{background:"orange"}},"Namasate H2");
-const container = React.createElement("div",{id:"container"},[heading1,heading2]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
